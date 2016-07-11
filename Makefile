ac4678b188e056b05c3c5fc907fa07ca79e13500
Felix Salfelder <felix@salfelder.org>
2016-07-11 16:47:39 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,53 @@
+GNUCAP_CONF = $(shell which gnucap-conf$(SUFFIX))
+
+include Make.override
+
+ifneq ($(GNUCAP_CONF),)
+    CXX = $(shell $(GNUCAP_CONF) --cxx)
+    GNUCAP_CPPFLAGS = $(shell $(GNUCAP_CONF) --cppflags) -DADD_VERSION -DPIC
+    GNUCAP_CXXFLAGS = $(shell $(GNUCAP_CONF) --cxxflags)
+	 GNUCAP_LIBDIR   = $(shell $(GNUCAP_CONF) --libdir)
+else
+    $(info no gnucap-conf, this might not work)
+    CXX = g++
+    GNUCAP_CXXFLAGS = \
+        -g -O0 \
+        -Wall -Wextra \
+        -Wswitch-enum -Wundef -Wpointer-arith -Woverloaded-virtual \
+        -Wcast-qual -Wcast-align -Wpacked -Wshadow -Wconversion \
+        -Winit-self -Wmissing-include-dirs -Winvalid-pch \
+        -Wvolatile-register-var -Wstack-protector \
+        -Wlogical-op -Wvla -Woverlength-strings -Wsign-conversion
+    GNUCAP_CPPFLAGS = \
+        -DHAVE_LIBREADLINE \
+        -DUNIX \
+        -DTRACE_UNTESTED
+	 GNUCAP_LIBDIR=/usr/share/gnucap
+endif
+
+GNUCAP_CXXFLAGS+= -fPIC -shared
+
+CLEANFILES = f_random.so
+INSTALL_FILES = f_random.so
+
+all: f_random.so
+
+LIBS=-lgsl -lgslcblas -lm
+f_random.so : f_random.cc
+	$(CXX) $(CXXFLAGS) $(GNUCAP_CXXFLAGS) $(CPPFLAGS) $(GNUCAP_CPPFLAGS) -o $@ f_random.cc $(LIBS)
+
+install : $(INSTALL_FILES)
+	install -d $(GNUCAP_LIBDIR)
+	install $(INSTALL_FILES) $(GNUCAP_LIBDIR)
+
+uninstall :
+	(cd $(GNUCAP_LIBDIR) ; rm $(INSTALL_FILES))
+
+clean :
+	rm -f $(CLEANFILES)
+
+distclean: clean
+	rm Make.override
+
+Make.override:
+	[ -e $@ ] || echo "# here you may override settings" > $@
---
+++ f_random.cc
@@ -0,0 +1,117 @@
+/*                             -*- C++ -*-
+ * Copyright (C) 2016 Felix Salfelder
+ * Author: same
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "u_parameter.h"
+#include "u_function.h"
+# include <gsl/gsl_randist.h>
+
+namespace{
+
+static gsl_rng *_rng;
+
+class agauss : public FUNCTION { //
+public:
+	fun_t eval(CS& Cmd, const CARD_LIST* Scope)const
+	{
+		{
+			const gsl_rng_type * T;
+			T = gsl_rng_default;
+
+			if (!_rng){
+				gsl_rng_env_setup();
+				_rng = gsl_rng_alloc(T);
+			}
+		}
+		PARAMETER<double> mean;
+		PARAMETER<double> abs_dev_in; // standard deviation. NOT variance
+		PARAMETER<double> sigma; // some weird scaling. NOT standard deviation
+		Cmd >> mean >> abs_dev_in;
+		if(!Cmd.more()){ untested();
+		}else{ untested();
+			Cmd >> sigma;
+			sigma.e_val(NOT_INPUT, Scope);
+		}
+		mean.e_val(NOT_INPUT, Scope);
+		abs_dev_in.e_val(NOT_INPUT, Scope);
+		double abs_dev = abs_dev_in;
+
+		if(!sigma.has_hard_value()){ untested();
+		}else if(double(sigma)==0){
+			incomplete();
+		}else{ untested();
+			abs_dev = abs_dev/sigma;
+		}
+
+		return gsl_ran_gaussian(_rng, abs_dev) + mean;
+	}
+private:
+} p_gauss;
+DISPATCHER<FUNCTION>::INSTALL d_gauss(&function_dispatcher, "agauss", &p_gauss);
+
+class rgauss : public FUNCTION { //
+public:
+	fun_t eval(CS& Cmd, const CARD_LIST* Scope)const
+	{
+		{
+			const gsl_rng_type * T;
+			T = gsl_rng_default;
+
+			if (!_rng){
+				gsl_rng_env_setup();
+				_rng = gsl_rng_alloc(T);
+			}
+		}
+		PARAMETER<double> mean;
+		PARAMETER<double> rel_dev; // relative std deviation. NOT variance
+		PARAMETER<double> sigma; // some weird scaling. NOT standard deviation
+		Cmd >> mean >> rel_dev;
+		mean.e_val(NOT_INPUT, Scope);
+		rel_dev.e_val(NOT_INPUT, Scope);
+		if(!Cmd.more()){ untested();
+		}else{ untested();
+			Cmd >> sigma;
+			sigma.e_val(NOT_INPUT, Scope);
+		}
+		mean.e_val(NOT_INPUT, Scope);
+
+		double abs_dev = 0;
+
+		if(!mean.has_hard_value()){
+			incomplete();
+		}else{ untested();
+			abs_dev = rel_dev*mean;
+		}
+
+		if(!sigma.has_hard_value()){ untested();
+		}else if(double(sigma)==0){
+			incomplete();
+		}else{
+			abs_dev = abs_dev/sigma;
+		}
+
+		return gsl_ran_gaussian(_rng, abs_dev) + mean;
+	}
+private:
+} p_rgauss;
+DISPATCHER<FUNCTION>::INSTALL d_rgauss(&function_dispatcher, "gauss|rgauss", &p_rgauss);
+
+}
